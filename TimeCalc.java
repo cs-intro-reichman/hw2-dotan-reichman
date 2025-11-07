a8736ff90209a3617199ff609bfb4713941a0145
Dotan Reichman <dotanreichman@Dotans-MacBook-Pro.local>
2025-11-07 16:32:30 +0200

    Testing

--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,19 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
+        int hours= Integer.parseInt(args[0]);
+        int minutes= Integer.parseInt(args[1]);
+        int minutesToAdd= Integer.parseInt(args[2]);
+        int totalMinutes= hours * 60 + minutes + minutesToAdd;
+        int totalHours= (totalMinutes / 60);
+        int newHours=  totalHours % 24;
+        int newminutes= totalMinutes -(totalHours * 60);
+        if(newHours<10){
+            if(minutes<10)
+                System.out.println("0"+newHours+":0"+newminutes);
+        else 
+            System.out.println("0"+newHours+":"+newminutes);
+        }
+        else   
+            System.out.println(newHours + ":" + newminutes);
     }
 }
