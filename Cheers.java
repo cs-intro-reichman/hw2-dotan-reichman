76b7f42b22115aff157cced710c7a0abc1c449e1
Dotan Reichman <dotanreichman@Dotans-MacBook-Pro.local>
2025-11-12 19:27:40 +0200

    Test

--- Cheers.java
+++ Cheers.java
@@ -8,6 +8,22 @@ Great work boaz!
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+                String str = args[0];
+        int amount = Integer.parseInt(args[1]);
+                String an = "AEFHILMNORSXaefhilmnorsx";
+                str= str.toUpperCase();
+            for (int i = 0 ; i<str.length() ; i++){
+                char c = str.charAt(i);
+                if (an.indexOf(c)==-1) { System.out.println("Give me a  " + (str.charAt(i)) + ": " + str.charAt(i) + "!");}
+                  else { System.out.println("Give me an " + str.charAt(i) + ": " + str.charAt(i) + "!");       
+        
+            }
+        }
+                System.out.println("What does that spell?");
+
+        for (int i = 0; i<amount; i++){
+                System.out.println(str + "!!!");
+        }
+
         }
 }
