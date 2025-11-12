9946e24d1e7498e4bea075e02bc26940329b1607
Dotan Reichman <dotanreichman@Dotans-MacBook-Pro.local>
2025-11-12 18:08:41 +0200

    done

--- TimeCalc.java
+++ TimeCalc.java
@@ -1,8 +1,9 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        int hours= Integer.parseInt(args[0]);
-        int minutes= Integer.parseInt(args[1]);
-        int minutesToAdd= Integer.parseInt(args[2]);
+        String time=args[0];
+        int hours= time.charAt(0)*10+time.charAt(1);
+        int minutes= time.charAt(3)*10+time.charAt(4);
+        int minutesToAdd= Integer.parseInt(args[1]);
         int totalMinutes= hours * 60 + minutes + minutesToAdd;
         int totalHours= (totalMinutes / 60);
         int newHours=  totalHours % 24;
