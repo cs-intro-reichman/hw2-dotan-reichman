ffc5226ff21a4d9d701955d36d7dc564b94c4329
Dotan Reichman <dotanreichman@Dotans-MacBook-Pro.local>
2025-11-13 23:15:23 +0200

    test finale

--- TimeCalc.java
+++ TimeCalc.java
@@ -1,25 +1,21 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        String time=args[0];
-        int hours= time.charAt(0)*10+time.charAt(1);
-        int minutes= time.charAt(3)*10+time.charAt(4);
-        int minutesToAdd= Integer.parseInt(args[1]);
-        int totalMinutes= hours * 60 + minutes + minutesToAdd;
-        int totalHours= (totalMinutes / 60);
-        int newHours=  totalHours % 24;
-        int newminutes= totalMinutes -(totalHours * 60);
-        if(newHours<10){
-            if(minutes<10)
-                System.out.println("0"+newHours+":0"+newminutes);
-        else 
-            System.out.println("0"+newHours+":"+newminutes);
+        int hours = Integer.parseInt("" + args [0].charAt(0) + args [0].charAt(1));
+        int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
+        int minutesToAdd = Integer.parseInt(args[1]);
+        int totalMinutes = (hours * 60) + minutes + minutesToAdd;
+        int totalHours = (totalMinutes / 60);
+        int newHours = totalHours % 24;
+        int newMinutes = totalMinutes - (totalHours * 60);
+        if (newHours >= 10) { 
+            System.out.print(newHours + ":");
+        } else {
+            System.out.print("0" + newHours + ":");
         }
-        else{
-            if(minutes<10)
-             System.out.println(newHours+":0"+newminutes);
-            else
-                 System.out.println(newHours + ":" + newminutes);
-
-        }   
+        if (newMinutes >= 10) {
+            System.out.print(newMinutes); 
+        } else {
+            System.out.print("0" + newMinutes); 
+        }
     }
 }
